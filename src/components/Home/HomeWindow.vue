f660e48b3c1661947c5237bdd04d8325e9852f17
SamandarYigitaliyev <uzgrandmaster@gmail.com>
2023-10-26 07:52:44 +0500

    Created files

---
+++ src/components/Home/HomeWindow.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="fixed left-2 top-2">
+        <Header></Header>
+        <Saidbar></Saidbar>
+    </div>
+    <div class="right-10">
+        <div class="p-4 sm:ml-64">
+            <div class="p-4 mt-14">
+                <main>
+                  <SendMessage></SendMessage>
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Header from './Header.vue'
+import Saidbar from './Saidbar.vue'
+import SendMessage from './SendMessage.vue';
+
+export default {
+    components:
+    {
+        Header,
+        Saidbar,
+        SendMessage,
+    }
+}
+</script>
+<style ></style>
---
+++ src/views/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import FlowbiteSetup from './FlowbiteSetup.vue'
+import HomeWindow from '../components/Home/HomeWindow.vue';
+</script>
+
+<template>
+    <HomeWindow></HomeWindow>
+    <FlowbiteSetup></FlowbiteSetup>
+</template>
+
+<style scoped></style>
